ed0afe6e8dd9a48f03db9fe0a55a78fb8f898dfe
250542024 <250542024@firat.edu.tr>
2025-10-24 01:25:27 +0300

    Add AsciiArtHarf class to display letter 'S'

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,15 @@
+package StringDesenler;
+
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println("S harfi:");
+        
+        System.out.println(" ******** ");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println(" ******** ");
+        System.out.println("        *");
+        System.out.println("        *");
+        System.out.println(" ******** ");
+    }
+}
