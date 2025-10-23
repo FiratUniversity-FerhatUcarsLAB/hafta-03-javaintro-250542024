566b6cafaf19c9e514b879ba3e16d7c4a521e4ad
250542024 <250542024@firat.edu.tr>
2025-10-24 01:20:10 +0300

    Add BesKereYaz class to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,7 @@
+public class BesKereYaz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Java’ya Hos Geldiniz");
+        }
+    }
+}
